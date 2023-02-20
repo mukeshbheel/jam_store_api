4e813926d3f7b73b892523c89b0b8f971680a8b0
mukesh-addweb <mukesh@addwebsolution.in>
2023-02-20 12:27:03 +0530

    api done

--- models/product.js
+++ models/product.js
@@ -19,15 +19,19 @@ const ProductSchema = new mongoose.Schema({
         required: true
     },
     size: {
-        type: String,
+        type: Array,
     },
     color: {
-        type: String,
+        type: Array,
     },
     price: {
         type: Number,
         required: true
     },
+    inStock: {
+        type: Boolean,
+        default: true
+    }
 }, {
     timestamps: true
 });
